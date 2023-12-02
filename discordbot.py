e33b2bc5ad905870ee14affaf94a7f0a9f673cbf
uninin <125956679+uninin3141@users.noreply.github.com>
2023-12-02 18:56:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,26 +1,35 @@
 import discord
-import traceback
-from discord.ext import commands
-from os import getenv
+import os
 
+# Railwayの環境変数からDiscord Botのトークンを取得
+TOKEN = os.environ["DISCORD_BOT_TOKEN"]
+
+# インテントの生成
 intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
 intents.message_content = True
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+@client.event
+async def on_ready():
+    print(f'ログインしました: {client.user}')
 
+@client.event
+async def on_member_update(before,after):
+    # 特定のユーザーのIDを指定
+    specific_user_id = 932667896776237057  # 特定のユーザーのIDをここに入力
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    # 変更が特定のユーザーに対して行われたか確認
+    if after.id == specific_user_id:
+        # ニックネームが変更されたか確認
+        if before.nick != after.nick:
+            try:
+                # ニックネームを"abc"に変更
+                await after.edit(nick="【MCキング】チャリ【VCは俺が主役】")
+                print(f"{after.display_name}の名前を変更しました。")
+            except Exception as e:
+                print(f"名前の変更に失敗しました: {e}")
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
-
+# ここにBotのトークンを入力
+client.run(TOKEN)
